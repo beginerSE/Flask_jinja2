e58141dea2d3383b4e5f8052b03e9d68083200e2
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-11-07 11:49:31 +0900

    エラーを修正

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def test_jinja():
+def test_jinja2():
     return render_template('test_jinja2.html', name="taro")
-    
-#テスト環境起動
+
+
+# 追加した処理
+@app.route('/test_ifa')
+def if_testa(name=None):
+    name = 'jiro'
+    return render_template('test_if.html', name=name)
+
+
+@app.route('/test_ifb')
+def if_testb(name=None):
+    name = 'sumijiro'
+    return render_template('test_if.html', name=name)
+
+
+# テスト環境起動
 app.run(debug=True)
