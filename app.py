ab04084a554a685156378dc0b7dc87876923204b
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-11-07 12:01:21 +0900

    エラーを修正

--- app.py
+++ app.py
@@ -7,17 +7,25 @@ def test_jinja2():
     return render_template('test_jinja2.html', name="taro")
 
 
-# 追加した処理
+# if文処理(True)
 @app.route('/test_ifa')
 def if_testa(name=None):
     name = 'jiro'
     return render_template('test_if.html', name=name)
 
 
+# if文処理(False)
 @app.route('/test_ifb')
 def if_testb(name=None):
-    name = 'sumijiro'
+    name = 'taro'
     return render_template('test_if.html', name=name)
+
+
+# for文処理
+@app.route('/test_for')
+def test_for():
+    brothers = ["太郎", "次郎", "三郎"]
+    return render_template('test_for.html', users=brothers)
 
 
 # テスト環境起動
